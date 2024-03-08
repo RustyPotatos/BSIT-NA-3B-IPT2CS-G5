b3eedd696bf9a1d9a037f5ea6a5432b6e5f24aa9
NicsDg <160439664+NicsDg@users.noreply.github.com>
2024-03-08 20:33:52 +0800

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-var btnMode = document.querySelector('.btn-mode');
-            btnMode.onclick = function(){
-                document.body.classList.toggle("white-theme")
-                btnMode.classList.toggle("fa-sun")
-                btnMode.classList.toggle("fa-moon")
-            }
