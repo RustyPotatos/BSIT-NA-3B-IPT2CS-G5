07697a1cd89ef96f1078e90c1371c67fac3f9e14
Harold <haroldpineda0720@gmail.com>
2024-03-08 22:01:11 +0800

    final javascript

--- Pineda.js
+++ Pineda.js
@@ -1,1 +1,10 @@
-js file ni harold
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box', { origin: 'bottom' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.about-content', { origin: 'right' });
+ScrollReveal().reveal('.portfolio', { origin: 'left' });
